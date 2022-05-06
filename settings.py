c631eaaae266cdbdfb3beb1eb4ca14ac89ebf5fe
PavelMayorov <pavelmayorov1996@gmail.com>
2022-05-06 16:31:05 +0500

    added settings

---
+++ settings.py
@@ -0,0 +1,10 @@
+from openpyxl.styles import PatternFill, Alignment
+
+RED = PatternFill(start_color="FF1E3A", end_color="FF1E3A", fill_type="solid")
+GREEN = PatternFill(start_color="7CFC00", end_color="7CFC00", fill_type="solid")
+CENTER = Alignment(horizontal='center')
+
+HEADERS = {
+    'x-auth-token': 'af1e65517d8bc65a1deb5404048e011d',
+    'Content-Type': 'application/x-www-form-urlencoded',
+}
