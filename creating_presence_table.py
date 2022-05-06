78025c25b15feb41730bfdb42f3ed68dcce0aac3
PavelMayorov <pavelmayorov1996@gmail.com>
2022-05-06 16:27:46 +0500

    new commit

--- creating_presence_table.py
+++ creating_presence_table.py
@@ -1,12 +1,7 @@
 import openpyxl
-from openpyxl.styles import PatternFill, Alignment
 
-from extract_statistic_to_csv import collection_data_all_events
-
-
-RED = PatternFill(start_color="FF1E3A", end_color="FF1E3A", fill_type="solid")
-GREEN = PatternFill(start_color="7CFC00", end_color="7CFC00", fill_type="solid")
-CENTER = Alignment(horizontal='center')
+from extract_statistic_to_csv import collection_data_all_events, user_data_request
+from settings import RED, GREEN, CENTER
 
 
 def input_file_name():
@@ -19,37 +14,42 @@ def input_file_name():
             return file_name
 
 
-def cell_entry(row, column, value, fill, alignment):
+def cell_entry(sheet, row, column, value, fill, alignment):
     cell = sheet.cell(row=row, column=column)
     cell.value = value
     cell.fill = fill
     cell.alignment = alignment
 
 
-events_data = collection_data_all_events()
-file_name = input_file_name()
-try:
-    students_list = openpyxl.load_workbook(filename=file_name)
-except Exception as ex:
-    print("Не удалось открыть файл! Проверьте имя файла и его расположение.")
-    print(ex)
-else:
-    sheet = students_list['Лист1']
-    row = 1
-    column = 4
-    for event_data in events_data:
-        date = event_data['date']
-        if sheet.cell(row=row, column=column).value != date:
-            column += 1
-            sheet.cell(row=row, column=column).value = date
-            sheet.column_dimensions[chr(64+column)].width = 10
-        for i in range(2, 50):
-            if event_data['email'].lower() == str(sheet.cell(row=row+i, column=4).value).lower():
-                if event_data['actual_presence']:
+def writing_to_file(file_name, events_data):
+    try:
+        students_list = openpyxl.load_workbook(filename=file_name)
+    except Exception as ex:
+        print("Не удалось открыть файл! Проверьте имя файла и его расположение.")
+        print(ex)
+    else:
+        sheet = students_list['Лист1']
+        row = 1
+        column = 4
+        for event_data in events_data:
+            date = event_data['date']
+            if sheet.cell(row=row, column=column).value != date:
+                column += 1
+                sheet.cell(row=row, column=column).value = date
+                sheet.column_dimensions[chr(64+column)].width = 10
+            for i in range(2, 50):
+                if not sheet.cell(row=row+i, column=column).value and sheet.cell(row=row+i, column=1).value:
+                    cell_entry(sheet, row+i, column, 'н', RED, CENTER)
+                if event_data['email'].lower() == str(sheet.cell(row=row+i, column=4).value).lower():
                     if event_data['actual_presence'] == '100,00%' or event_data['actual_presence'] >= '60,00%':
-                        cell_entry(row+i, column, '+', GREEN, CENTER)
+                        cell_entry(sheet, row+i, column, '+', GREEN, CENTER)
                     else:
-                        cell_entry(row+i, column, '-', RED, CENTER)
-                else:
-                    cell_entry(row+i, column, 'н', RED, CENTER)
-    students_list.save(file_name)
+                        cell_entry(sheet, row+i, column, '-', RED, CENTER)
+        students_list.save(file_name)
+
+
+if __name__ == '__main__':
+    search_keyword, search_date_from, search_date_to = user_data_request()
+    events_data = collection_data_all_events(search_keyword, search_date_from, search_date_to)
+    file_name = input_file_name()
+    writing_to_file(file_name, events_data)
--- extract_statistic_to_csv.py
+++ extract_statistic_to_csv.py
@@ -1,23 +1,20 @@
 import requests
 import pandas as pd
 
-# search_keyword = input("Введите ключевое слово для поиска мепроприятий: ")
-# search_date_from = input("Введите дату начала выборки в формате 'ГГГГ-ММ-ДД': ")
-# search_date_to = input("Введите дату конца выборки в формате 'ГГГГ-ММ-ДД': ")
-search_keyword = 'СТ-21-03/04'
-search_date_from = '2022-03-01'
-search_date_to = '2022-04-12'
+from settings import HEADERS
 
-headers = {
-    'x-auth-token': 'af1e65517d8bc65a1deb5404048e011d',
-    'Content-Type': 'application/x-www-form-urlencoded',
-}
+
+def user_data_request():
+    search_keyword = input("Введите ключевое слово для поиска мепроприятий: ")
+    search_date_from = input("Введите дату начала выборки в формате 'ГГГГ-ММ-ДД': ")
+    search_date_to = input("Введите дату конца выборки в формате 'ГГГГ-ММ-ДД': ")
+    return search_keyword, search_date_from, search_date_to
 
 
 def get_id_all_events(date_from, date_to, keyword):
     url_events = f'https://userapi.webinar.ru/v3/organization/events/schedule?from={date_from}&to={date_to}' \
                  f'&perPage=100&page=1&status[0]=STOP&name={keyword}'
-    events_response = requests.get(url_events, headers=headers).json()
+    events_response = requests.get(url_events, headers=HEADERS).json()
     events_id = []
     for event in events_response:
         events_id.append(event['id'])
@@ -27,7 +24,7 @@ def get_id_all_events(date_from, date_to, keyword):
 
 def get_event_data(date_from, date_to, id):
     url_event = f'https://userapi.webinar.ru/v3/stats/users?from={date_from}&to={date_to}&eventId={id}'
-    event_response = requests.get(url_event, headers=headers).json()
+    event_response = requests.get(url_event, headers=HEADERS).json()
     data = []
     for pos, item in enumerate(event_response):
         data.append({})
@@ -41,16 +38,16 @@ def get_event_data(date_from, date_to, id):
     return data
 
 
-def collection_data_all_events():
+def collection_data_all_events(search_keyword, search_date_from, search_date_to):
     events_id = get_id_all_events(search_date_from, search_date_to, search_keyword)
     events_data = []
     for event_id in events_id:
-        event_data = get_event_data(search_date_from, search_date_to, event_id)
-        events_data += event_data
+        events_data += get_event_data(search_date_from, search_date_to, event_id)
     return events_data
 
 
 if __name__ == "__main__":
-    events_data = collection_data_all_events()
+    search_keyword, search_date_from, search_date_to = user_data_request()
+    events_data = collection_data_all_events(search_keyword, search_date_from, search_date_to)
     df = pd.DataFrame(events_data)
     df.to_csv('statistic.csv')
